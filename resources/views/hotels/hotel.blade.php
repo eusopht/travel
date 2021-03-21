fb540b49d531b59855cb8da3cf2ffe2f088b538c
owaisakbar <muhammad.owais.akbar@gmail.com>
2021-03-21 23:22:44 +0500

    Comit changes

--- resources/views/hotels/hotel.blade.php
+++ resources/views/hotels/hotel.blade.php
@@ -39,7 +39,7 @@
                 <ul>
                     <li><a class="nav-link-flight" href="{{ route('home') }}">Flights</a></li>
                     <li><a class="active nav-link-hotel" href="{{ route('hotelView') }}">Hotels</a></li>
-                    <li><a class="nav-link-car" href="{{ route('carView') }}">Car Hire</a></li>
+                    <li><a class="nav-link-car" href="{{ route('carView') }}">Harmain Transport</a></li>
                 </ul>
             </div>
             <div class="form-main">
@@ -136,6 +136,44 @@
         </div>
         </div>
     </section>
+
+    
+    <section class="cities-section">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="hed  text-center">
+                    <h2>Trending Cities</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa.</p>
+                </div>
+
+                <div class="body col-md-12">
+                    <div class="row">
+                        <div class="cities-box col-md-4">
+                            <a href="#">
+                                <img src="assets/images/cities/1.jpg" alt="">
+                            </a>
+                        </div>
+                        
+                        <div class="col-md-4 cities-box cities--2">
+                            <a href="#">
+                                <img src="assets/images/cities/2.jpg" alt="">
+                            </a>
+                            <a href="#">
+                                <img src="assets/images/cities/3.jpg" alt="">
+                            </a>
+                        </div>
+                        
+                        <div class="col-md-4 cities-box">
+                            <a href="#">
+                                <img src="assets/images/cities/4.jpg" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <script src="{{ asset('assets/js/lib.js') }}"></script>
     <script type="text/javascript"
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0qe-Nm-I-wRVSHg__FQmbIIE9WNpbqms&amp;libraries=places">
